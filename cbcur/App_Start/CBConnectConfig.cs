39518a29687be1f566878e72d6373556d4a1524c
Bulat Zamilov <b.zamilov@innopolis.ru>
2016-10-07 15:46:48 +0300

    Made working init page

---
+++ cbcur/App_Start/CBConnectConfig.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+
+namespace cbcur.App_Start
+{
+    public class CB
+    {
+        public static ru.cbr.www.DailyInfo dailyInfo { get; private set; }
+
+        static CB()
+        {
+            dailyInfo = new ru.cbr.www.DailyInfo();
+            DataSet ds = dailyInfo.EnumValutes(Seld: false);
+            currencies = new Currency[ds.Tables[0].Rows.Count];
+            
+            for (int i = 0; i < currencies.Length; i++)
+            {
+                var currency = new Currency(ds.Tables[0].Rows[i].ItemArray[0] as string,
+                    ds.Tables[0].Rows[i].ItemArray[2] as string,
+                    ds.Tables[0].Rows[i].ItemArray[3] as string);
+                currencies[i] = currency;
+            }
+        }
+
+        private static Currency[] currencies;
+
+
+        public static Currency[] CurrencyArray
+        {
+            get { return currencies; }
+            set { currencies = value; }
+        }
+    }
+
+
+    public class Currency
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string RateBase { get; set; }
+
+        public Currency(string code, string name, string ratebase)
+        {
+            Code = code.Trim();
+            Name = name.Trim();
+            RateBase = (ratebase != null) ? ratebase.Trim(): ratebase;
+        }
+
+        public Tuple<DateTime, decimal>[] GetRate(DateTime from, DateTime to)
+        {
+            var days = (to - from).Days;
+            Tuple<DateTime, decimal>[] res = new Tuple<DateTime, decimal>[31];
+            DataSet dayrate = CB.dailyInfo.GetCursDynamic(from, to, Code);
+            for (int i = 0, j =0; i < days; i++, j++)
+            {
+                var day = (DateTime)dayrate.Tables[0].Rows[j].ItemArray[0];
+                var rate = (decimal)dayrate.Tables[0].Rows[j].ItemArray[3];
+                res[i] = Tuple.Create(day, rate);
+                if (day.DayOfWeek == DayOfWeek.Saturday & i != days - 1)
+                {
+                    res[++i] = Tuple.Create(new DateTime(day.Year, day.Month, day.Day + 1), rate);
+                    if (i != days - 1)
+                        res[++i] = Tuple.Create(new DateTime(day.Year, day.Month, day.Day + 2), rate);
+                }
+            }
+            return res;
+        }
+    }
+}
--- cbcur/Controllers/HomeController.cs
+++ cbcur/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,22 +9,120 @@ namespace cbcur.Controllers
 {
     public class HomeController : Controller
     {
+        public class GraphGenerator
+        {
+            private string graph = @"$(function () {
+    $('#curgraph').highcharts({
+        title: {
+            text: '{0}',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }]
+    });
+});
+";
+            public string GetGraph()
+            {
+                return graph;
+            }
+        }
+
+
         public ActionResult Index()
         {
+            var graph = new GraphGenerator();
+            Tuple<DateTime, decimal>[] currencyrate = {Tuple.Create(DateTime.Today, (decimal)0)};
+
+            //ViewBag.CurrencyGraph = graph.GetGraph();
+            DateTime today = DateTime.Today;
+            var monthdayscount = DateTime.DaysInMonth(today.Year, today.Month);
+            string[] monthdays = new string[monthdayscount];
+            for (int k = 1; k <= monthdayscount; k++)
+                monthdays[k - 1] = k.ToString();
+            ViewBag.Days = monthdays;
+            
+            //DataSet ds = cbcur.App_Start.CB.dailyInfo.EnumValutes(Seld: true);
+            List<SelectListItem> curnames = new List<SelectListItem>();
+            var i = 0;
+            foreach (var currency in App_Start.CB.CurrencyArray)
+            {
+                // US Dollar as default
+                if (currency.Code == "R01235")
+                {
+                    //curnames.Add(new SelectListItem { Text = currency.Name, Value = (++i).ToString(), Selected = true });
+                    curnames.Add(new SelectListItem { Text = currency.Name, Value = currency.Code, Selected = true });
+                    ViewBag.CurrencyTitle = currency.Name;
+                    currencyrate = currency.GetRate(new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1), DateTime.Today);
+                }
+                else
+                {
+                    //curnames.Add(new SelectListItem { Text = currency.Name + " " + currency.Code, Value = (++i).ToString() });
+                    curnames.Add(new SelectListItem { Text = currency.Name, Value = currency.Code });
+
+                }
+            }
+            ViewBag.CurrencyNames = curnames;
+
+            List<SelectListItem> months = new List<SelectListItem>();
+            months.Add(new SelectListItem { Text = "JAN", Value = "1" });
+            months.Add(new SelectListItem { Text = "FEB", Value = "2" });
+            months.Add(new SelectListItem { Text = "MAR", Value = "3" });
+            months.Add(new SelectListItem { Text = "APR", Value = "4" });
+            months.Add(new SelectListItem { Text = "MAY", Value = "5" });
+            months.Add(new SelectListItem { Text = "JUN", Value = "6" });
+            months.Add(new SelectListItem { Text = "JUL", Value = "7" });
+            months.Add(new SelectListItem { Text = "AUG", Value = "8" });
+            months.Add(new SelectListItem { Text = "SEP", Value = "9" });
+            months.Add(new SelectListItem { Text = "OCT", Value = "10" });
+            months.Add(new SelectListItem { Text = "NOV", Value = "11" });
+            months.Add(new SelectListItem { Text = "DEC", Value = "12" });
+            ViewBag.Months = months;
+            ViewBag.Month = DateTime.Now.ToString("MMM");
+          
+            
+            string[] rates = new string[currencyrate.Length];
+            for(int l=0;l<currencyrate.Length; l++)
+            {
+                if (currencyrate[l] != null)
+                    rates[l] = currencyrate[l].Item2.ToString().Replace(',','.');
+            }
+            ViewBag.CurrencyRates = rates;
+
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult UpdateChart()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
